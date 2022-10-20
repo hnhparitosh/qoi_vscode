232d3d6cc7106b6982996625b2c9ff6084b88cba
paritoshdahiya <paritosh@syncsense.ai>
2022-10-20 07:34:20 +0530

    added preview logic

---
+++ src/qoiPreview.ts
@@ -0,0 +1,124 @@
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { Disposable } from './disposable';
+
+function escapeAttribute(value: string | vscode.Uri): string {
+  return value.toString().replace(/"/g, '&quot;');
+}
+
+type PreviewState = 'Disposed' | 'Visible' | 'Active';
+
+export class QoiPreview extends Disposable {
+  private _previewState: PreviewState = 'Visible';
+
+  constructor(
+    private readonly extensionRoot: vscode.Uri,
+    private readonly resource: vscode.Uri,
+    private readonly webviewEditor: vscode.WebviewPanel) {
+    super();
+    const resourceRoot = resource.with({
+      path: resource.path.replace(/\/[^/]+?\.\w+$/, '/'),
+    });
+
+    webviewEditor.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [resourceRoot, extensionRoot],
+    };
+
+    this._register(
+      webviewEditor.webview.onDidReceiveMessage((message) => {
+        switch (message.type) {
+          case 'reopen-as-text': {
+            vscode.commands.executeCommand(
+              'vscode.openWith',
+              resource,
+              'default',
+              webviewEditor.viewColumn
+            );
+            break;
+          }
+        }
+      })
+    );
+
+    this._register(
+      webviewEditor.onDidChangeViewState(() => {
+        this.update();
+      })
+    );
+
+    this._register(
+      webviewEditor.onDidDispose(() => {
+        this._previewState = 'Disposed';
+      })
+    );
+
+    const watcher = this._register(
+      vscode.workspace.createFileSystemWatcher(resource.fsPath)
+    );
+    this._register(
+      watcher.onDidChange((e) => {
+        if (e.toString() === this.resource.toString()) {
+          this.reload();
+        }
+      })
+    );
+    this._register(
+      watcher.onDidDelete((e) => {
+        if (e.toString() === this.resource.toString()) {
+          this.webviewEditor.dispose();
+        }
+      })
+    );
+
+    this.webviewEditor.webview.html = this.getWebviewContents();
+    this.update();
+  }
+
+  private reload(): void {
+    if (this._previewState !== 'Disposed') {
+      this.webviewEditor.webview.postMessage({ type: 'reload' });
+    }
+  }
+
+  private update(): void {
+    if (this._previewState === 'Disposed') {
+      return;
+    }
+
+    if (this.webviewEditor.active) {
+      this._previewState = 'Active';
+      return;
+    }
+    this._previewState = 'Visible';
+  }
+
+  private getWebviewContents(): string {
+    const webview = this.webviewEditor.webview;
+    const docPath = webview.asWebviewUri(this.resource);
+    const cspSource = webview.cspSource;
+    const resolveAsUri = (...p: string[]): vscode.Uri => {
+      const uri = vscode.Uri.file(path.join(this.extensionRoot.path, ...p));
+      return webview.asWebviewUri(uri);
+    };
+
+    const config = vscode.workspace.getConfiguration('qoi-preview');
+    const settings = {
+      cMapUrl: resolveAsUri('lib', 'web', 'cmaps/').toString(),
+      path: docPath.toString(),
+      defaults: {
+        cursor: config.get('default.cursor') as string,
+        scale: config.get('default.scale') as string,
+        sidebar: config.get('default.sidebar') as boolean,
+        scrollMode: config.get('default.scrollMode') as string,
+        spreadMode: config.get('default.spreadMode') as string,
+      },
+    };
+
+    const head = `html head`;
+    const body = `html body`;
+    const tail = `html tail`;
+
+    return head + body + tail;
+  }
+}
